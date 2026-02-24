a547404d8d91727c9773e58c29acc08dca256de2
lokesh15kl <2400032126@kluniversity.in>
2026-02-24 20:12:15 +0530

    Fix Vite base path for local development

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/career-assignment/",   // 👈 ADD THIS LINE
+  base: process.env.NODE_ENV === 'production' ? "/career-assignment/" : "/",
 
   plugins: [react()],
 
