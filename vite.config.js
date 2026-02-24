8ef35b21abd4f1c7ec10a46f1b1ebb35ab431deb
lokesh15kl <2400032126@kluniversity.in>
2026-02-24 17:17:04 +0530

    Add backend and Render deployment

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/career-assignment/",   // 👈 ADD THIS LINE
+
   plugins: [react()],
+
   server: {
     proxy: {
       '/api': {
